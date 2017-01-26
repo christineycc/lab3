4d301f59847d8c885cb4e2d4dc0b13630e2f73f4
Christine Chang <cyc058@ucsd.edu>
2017-01-26 10:29:00 -0800

    edited website

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,27 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Javascript connected");
+		$(".jumbotron p").addClass("active");
 	});
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) { 
+	console.log("Project clicked");
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).toggle();
+    }
 }
